a0c1d1fb3efe7d43e5fee9e4a8d50cd360242dec
jkatsanis <katsanis18@gmail.com>
2025-02-24 10:57:32 +0100

    uga

---
+++ src/main/java/org/example/entities/Ticket.java
@@ -0,0 +1,24 @@
+package org.example.entities;
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "ticket_typ", discriminatorType = DiscriminatorType.STRING)
+public abstract class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double preis;
+}
+
+@Entity
+@DiscriminatorValue("Tageskarte")
+class Tageskarte extends Ticket {}
+
+@Entity
+@DiscriminatorValue("Wochenkarte")
+class Wochenkarte extends Ticket {}
+
+@Entity
+@DiscriminatorValue("Saisonkarte")
+class Saisonkarte extends Ticket {}
