736683e7b5c200620e0b41d2d11f8de4a1f64a11
jkatsanis <katsanis18@gmail.com>
2025-04-06 22:38:35 +0200

    saug

---
+++ backend/src/main/java/at/htlleonding/entities/Piste.java
@@ -0,0 +1,19 @@
+package at.htlleonding.entities;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+class Piste {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String schwierigkeitsgrad;
+    private double laenge;
+
+    @ManyToOne
+    @JoinColumn(name = "skilift_id")
+    private SkiLift skiLift;
+
+    // Getter & Setter
+}
---
+++ backend/src/main/java/at/htlleonding/entities/Skilift.java
@@ -0,0 +1,18 @@
+package at.htlleonding.entities;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+class SkiLift {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String typ; // Sessellift, Schlepplift, Gondel
+    private int kapazität;
+
+    @OneToMany(mappedBy = "skiLift")
+    private List<Piste> pisten;
+
+    // Getter & Setter
+}
